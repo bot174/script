7c87a2ea798d1bf8e293f9ee29328b0e37f1aa69
Jayly <91819282+bot174@users.noreply.github.com>
2024-08-20 21:47:50 +0800

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -3,8 +3,16 @@ from pynbt import TAG_Compound, NBTFile, TAG_Byte, TAG_Int, TAG_Short, TAG_Strin
 
 file = 'extracted/uncensored_lib.mcstructure'
 
-def callback():
-  print('Done')
+def write(nbt: NBTFile):
+  print('Saving NBT...')
+    
+  with open(file=file, mode='wb') as io:
+    def write_callback(data: bytes):
+      io.write(data)
+
+    nbt.save(write_callback, True)
+
+  print('Saved NBT')
 
 with open(file=file, mode='rb') as io:
   print('Loaded file')
@@ -21,13 +29,13 @@ with open(file=file, mode='rb') as io:
     block_entity_data = block['block_entity_data']
     book = block_entity_data['book']
     written_book_content = book['components']['minecraft:written_book_content']
-    author = written_book_content['author']
-    pages: list[TAG_Compound] = []
-    custom_name = book['components']['minecraft:custom_name']
+    author = written_book_content['author'].value
+    pages = TAG_List(tag_type=TAG_Compound)
+    custom_name = book['components']['minecraft:custom_name'].value
     book_name = json.loads(custom_name)['extra'][0]['text']
 
     for page in written_book_content['pages']:
-      raw = json.loads(page['raw'])
+      raw = json.loads(page['raw'].value)
       text = ''.join(raw['extra'])
       page_bedrock = TAG_Compound({
         'photoname': TAG_String(''),
@@ -44,7 +52,7 @@ with open(file=file, mode='rb') as io:
         'tag': TAG_Compound({
           'author': TAG_String(author),
           'generation': TAG_Int(0),
-          'pages': TAG_List(pages),
+          'pages': pages,
           'title': TAG_String('§6' + book_name),
           'xuid': TAG_String('2535453759792258')
         })
@@ -54,13 +62,12 @@ with open(file=file, mode='rb') as io:
       'isMovable': TAG_Byte(1),
       'page': TAG_Int(0),
       'totalPages': TAG_Int(len(pages)),
-      'x': TAG_Int(block_entity_data['x']),
-      'y': TAG_Int(block_entity_data['y']),
-      'z': TAG_Int(block_entity_data['z'])
+      'x': TAG_Int(block_entity_data['x'].value),
+      'y': TAG_Int(block_entity_data['y'].value),
+      'z': TAG_Int(block_entity_data['z'].value)
     })
 
     block['block_entity_data'] = new_block_entity_data
+    print(nbt.pretty())
   
-  print('Saving NBT...')
-  nbt.save(callback, True)
-  print('Saved NBT')
+  write(nbt)
