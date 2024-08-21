b5001f6a014dd0b26b58adadbe3e766405d1aae4
Jayly <91819282+bot174@users.noreply.github.com>
2024-08-21 12:38:06 +0800

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -28,54 +28,56 @@ with open(file=file, mode='rb') as io:
 
     block = block_position_data[index]
     block_entity_data = block['block_entity_data']
-    book = block_entity_data['book']
-    written_book_content = book['components']['minecraft:written_book_content']
-    author = written_book_content['author'].value
-    pages = TAG_List(tag_type=TAG_Compound)
-    custom_name = book['components']['minecraft:custom_name'].value
-    book_name = json.loads(custom_name)['extra'][0]['text']
-
-    for page in written_book_content['pages']:
-      raw = json.loads(page['raw'].value)
-      extra: list[str] = raw['extra']
-      text = ''
-
-      for item in extra:
-        if (type(item) == str):
-          text += item
-        else:
-          text += item['text']
-
-      page_bedrock = TAG_Compound({
-        'photoname': TAG_String(''),
-        'text': TAG_String(text)
-      })
-      pages.append(page_bedrock)
     
-    new_block_entity_data = TAG_Compound({
-      'book': TAG_Compound({
-        'Count': TAG_Byte(1),
-        'Damage': TAG_Short(0),
-        'Name': TAG_String('minecraft:written_book'),
-        'WasPickedUp': TAG_Byte(0),
-        'tag': TAG_Compound({
-          'author': TAG_String(author),
-          'generation': TAG_Int(0),
-          'pages': pages,
-          'title': TAG_String('§6' + book_name),
-          'xuid': TAG_String('2535453759792258')
-        })
-      }),
-      'hasBook': TAG_Byte(1),
-      'id': TAG_String('Lectern'),
-      'isMovable': TAG_Byte(1),
-      'page': TAG_Int(0),
-      'totalPages': TAG_Int(len(pages)),
-      'x': TAG_Int(block_entity_data['x'].value),
-      'y': TAG_Int(block_entity_data['y'].value),
-      'z': TAG_Int(block_entity_data['z'].value)
-    })
+    if 'book' in block_entity_data:
+      book = block_entity_data['book']
+      written_book_content = book['components']['minecraft:written_book_content']
+      author = written_book_content['author'].value
+      pages = TAG_List(tag_type=TAG_Compound)
+      custom_name = book['components']['minecraft:custom_name'].value
+      book_name = json.loads(custom_name)['extra'][0]['text']
 
-    block['block_entity_data'] = new_block_entity_data
+      for page in written_book_content['pages']:
+        raw = json.loads(page['raw'].value)
+        extra: list[str] = raw['extra']
+        text = ''
+
+        for item in extra:
+          if (type(item) == str):
+            text += item
+          else:
+            text += item['text']
+
+        page_bedrock = TAG_Compound({
+          'photoname': TAG_String(''),
+          'text': TAG_String(text)
+        })
+        pages.append(page_bedrock)
+      
+      new_block_entity_data = TAG_Compound({
+        'book': TAG_Compound({
+          'Count': TAG_Byte(1),
+          'Damage': TAG_Short(0),
+          'Name': TAG_String('minecraft:written_book'),
+          'WasPickedUp': TAG_Byte(0),
+          'tag': TAG_Compound({
+            'author': TAG_String(author),
+            'generation': TAG_Int(0),
+            'pages': pages,
+            'title': TAG_String('§6' + book_name),
+            'xuid': TAG_String('2535453759792258')
+          })
+        }),
+        'hasBook': TAG_Byte(1),
+        'id': TAG_String('Lectern'),
+        'isMovable': TAG_Byte(1),
+        'page': TAG_Int(0),
+        'totalPages': TAG_Int(len(pages)),
+        'x': TAG_Int(block_entity_data['x'].value),
+        'y': TAG_Int(block_entity_data['y'].value),
+        'z': TAG_Int(block_entity_data['z'].value)
+      })
+
+      block['block_entity_data'] = new_block_entity_data
   
   write(nbt)
